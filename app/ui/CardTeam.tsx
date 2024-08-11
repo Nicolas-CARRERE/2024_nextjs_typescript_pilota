94a70594cc94d49487d4d556a8f9e0a3c0c15ccc
Nicolas Carrère <nicolascarrere.pro@gmail.com>
2024-08-11 10:56:57 +0200

    fix selection conditions to color selectedTeam cards

--- app/ui/CardTeam.tsx
+++ app/ui/CardTeam.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { link } from "fs";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   team: string;
@@ -10,27 +12,53 @@ type Props = {
 };
 
 const CardTeam = (props: Props) => {
+  const router = useRouter();
   const { team, playerA, playerB, score } = props;
+  const [hasSelectedTeam, setHasSelectedTeam] = React.useState(false);
 
   // Fonction pour obtenir la valeur d'un cookie
   const getCookieValue = (name: string) => {
     const match = document.cookie.match(
       new RegExp("(^| )" + name + "=([^;]+)")
     );
-    console.log("match", match);
     return match ? match[2] : null;
   };
 
   const handleClick = (e: React.MouseEvent) => {
-    // Mettre à jour le cookie selectedTeam avec l'équipe actuelle
-    document.cookie = `selectedTeam=${team}; path=/`;
-    console.log("selectedTeam", getCookieValue("selectedTeam"), team);
+    // Vérifier si l'équipe actuelle est déjà sélectionnée
+    const selectedTeam = getCookieValue("selectedTeam");
+
+    let newHasSelectedTeam;
+    if (selectedTeam === team) {
+      // Si l'équipe actuelle est déjà sélectionnée, définir hasSelectedTeam sur false
+      newHasSelectedTeam = !hasSelectedTeam;
+      console.log(
+        "selectedTeam",
+        selectedTeam,
+        "team",
+        team,
+        "newHasSelectedTeam",
+        newHasSelectedTeam
+      );
+    } else {
+      // Sinon, définir hasSelectedTeam sur true
+      newHasSelectedTeam = true;
+    }
+
+    setHasSelectedTeam(newHasSelectedTeam);
+    document.cookie = `hasSelectedTeam=${newHasSelectedTeam}; path=/`;
+    if (team) {
+      document.cookie = `selectedTeam=${team}; path=/`;
+    }
+
+    router.refresh();
   };
 
   return (
     <div
       className={
-        (getCookieValue(team) === team
+        (getCookieValue("selectedTeam") === team &&
+        getCookieValue("hasSelectedTeam") === "true"
           ? "bg-slate-400"
           : "hover:bg-slate-200") + " w-1/2 p-4 my-4 border rounded-xl"
       }
